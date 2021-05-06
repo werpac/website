a3be98379d958eb5aac9ec2973bc85ec17d0e606
werpac <werpac@163.com>
2021-05-06 13:15:47 +0800

    edit by hot_fix

--- src/com/pac/bean/SayHello.java
+++ src/com/pac/bean/SayHello.java
@@ -4,5 +4,6 @@ public class SayHello {
 	public static void main(String[] args) {
 		System.out.println("Hello,World!");
 		System.out.println("from werpac");
+		System.out.println("from new branch hot_fix");
 	}
 }
